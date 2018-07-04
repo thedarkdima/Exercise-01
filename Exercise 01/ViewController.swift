50ea09dc74b421a430b15aace5df14023b2bb143
Alona Tov <alonatov@alonas-macbook-air.local>
2018-07-04 18:40:49 +0300

    Initial Commit

---
+++ Exercise 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise 01
+//
+//  Created by Alona Tov on 04/07/2018.
+//  Copyright © 2018 PME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
